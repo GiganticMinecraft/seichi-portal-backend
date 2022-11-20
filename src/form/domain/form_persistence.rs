4250527e7ba6b5451b524ea519985fc4643f28aa
rito528 <rotoyutoriapp@gmail.com>
2022-11-20 13:38:22 +0900

    feat: create_form_listenerを実装

--- src/form/domain/form_persistence.rs
+++ src/form/domain/form_persistence.rs
@@ -1,11 +1,11 @@
 use crate::form::domain::{Form, FormId};
 
 /// formを生成する
-fn create_form(_form: &Form) {
+pub fn create_form(_form: Form) {
     todo!()
 }
 
 /// formを削除する
-fn delete_form(_form_id: &FormId) {
+pub fn delete_form(_form_id: &FormId) {
     todo!()
 }
--- src/form/listeners/form_listener.rs
+++ src/form/listeners/form_listener.rs
@@ -1,7 +1,8 @@
-use crate::form::domain::Form;
+use crate::form::domain::{create_form, Form};
 use actix_web::{post, web::Json, HttpResponse, Responder};
 
 #[post("/api/form/create")]
-async fn create_form(_info: Json<Form>) -> impl Responder {
+async fn create_form_listener(_info: Json<Form>) -> impl Responder {
+    create_form(_info.0);
     HttpResponse::Ok()
 }
