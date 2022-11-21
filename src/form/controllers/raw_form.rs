8d64f74b6e30c9202a2ed6e5c374b574e15692eb
rito528 <rotoyutoriapp@gmail.com>
2022-11-21 15:30:14 +0900

    fix: compile error

--- src/form/controllers/raw_form.rs
+++ src/form/controllers/raw_form.rs
@@ -14,7 +14,7 @@ impl From<RawForm> for Form {
             .form_titles(
                 f.form_titles()
                     .iter()
-                    .map(|t| FormTitle::builder().title(t.to_string()))
+                    .map(|t| FormTitle::builder().title(t.to_string()).build())
                     .collect(),
             )
             .form_id(FormId::builder().form_id(*f.form_id()).build())
