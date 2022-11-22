499cd402a65c41a58c940649f080a29db41bd3fb
rito528 <rotoyutoriapp@gmail.com>
2022-11-22 22:45:43 +0900

    feat: Questionをpubにする

--- src/form/handlers/domain_for_user_input/raw_question.rs
+++ src/form/handlers/domain_for_user_input/raw_question.rs
@@ -1,6 +1,6 @@
 use crate::form::handlers::domain_for_user_input::raw_question_type::QuestionType;
 
-struct Question {
+pub struct Question {
     title: String,
     description: String,
     question_type: QuestionType,
