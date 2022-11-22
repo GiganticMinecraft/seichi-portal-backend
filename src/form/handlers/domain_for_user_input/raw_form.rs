4ba43d60b91021370bce2b15b4dc3398055dee12
rito528 <rotoyutoriapp@gmail.com>
2022-11-22 23:06:59 +0900

    feat: RawFormにquestionsを追加

--- src/form/handlers/domain_for_user_input/raw_form.rs
+++ src/form/handlers/domain_for_user_input/raw_form.rs
@@ -1,10 +1,12 @@
 use crate::form::domain::{Form, FormId, FormName};
+use crate::form::handlers::domain_for_user_input::raw_question::Question;
 use derive_getters::Getters;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Getters)]
 pub struct RawForm {
     form_name: String,
+    questions: Vec<Question>,
 }
 
 // impl From<RawForm> for Form {
