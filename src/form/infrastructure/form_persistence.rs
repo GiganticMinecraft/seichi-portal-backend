074b7be8291a3bcebd727dca92ff45f7fee14c44
rito528 <rotoyutoriapp@gmail.com>
2022-11-23 12:54:11 +0900

    feat: create処理をtransactionで包む

--- src/form/infrastructure/form_persistence.rs
+++ src/form/infrastructure/form_persistence.rs
@@ -1,25 +1,28 @@
 use crate::database::connection::database_connection;
 use crate::form::handlers::domain_for_user_input::raw_form::RawForm;
 use crate::form::handlers::domain_for_user_input::raw_form_id::RawFormId;
+use diesel::result::Error;
 use diesel::sql_types::{Integer, Nullable, Text, VarChar};
-use diesel::{sql_query, MysqlConnection, QueryResult, RunQueryDsl};
+use diesel::{sql_query, Connection, MysqlConnection, QueryResult, RunQueryDsl};
 
 /// formを生成する
 pub fn create_form(_form: RawForm) -> bool {
     let connection: &mut MysqlConnection = &mut database_connection();
-    let is_form_inserted = sql_query("INSERT INTO seichi_portal.forms (name) VALUES (?)")
-        .bind::<VarChar, _>(_form.form_name())
-        .execute(connection)
-        .is_ok();
 
-    let created_form_id = sql_query("SELECT id FROM seichi_portal.forms WHERE name = ?")
-        .bind::<VarChar, _>(_form.form_name())
-        .get_result::<RawFormId>(connection)
-        .unwrap();
+    let transaction = connection.transaction(|connection| {
+        let is_form_inserted = sql_query("INSERT INTO seichi_portal.forms (name) VALUES (?)")
+            .bind::<VarChar, _>(_form.form_name())
+            .execute(connection)
+            .is_ok();
 
-    // NOTE: ここのid埋め込みは自動生成の数字なのでそのまま埋め込んで良い
-    let is_success_create_table = sql_query(format!(
-        r"CREATE TABLE forms.{} (
+        let created_form_id = sql_query("SELECT id FROM seichi_portal.forms WHERE name = ?")
+            .bind::<VarChar, _>(_form.form_name())
+            .get_result::<RawFormId>(connection)
+            .unwrap();
+
+        // NOTE: ここのid埋め込みは自動生成の数字なのでそのまま埋め込んで良い
+        let is_success_create_table = sql_query(format!(
+            r"CREATE TABLE forms.{} (
             id INT AUTO_INCREMENT,
             title VARCHAR(100),
             description VARCHAR(300),
@@ -28,27 +31,41 @@ pub fn create_form(_form: RawForm) -> bool {
             PRIMARY KEY(id)
         )
         ",
-        created_form_id.id()
-    ))
-    .execute(connection)
-    .is_ok();
-
-    let mut insert_state = _form.questions().iter().map(|question| {
-        let choices = question.choices().clone().map(|choices| choices.join(","));
-        sql_query(format!(
-            r"INSERT INTO forms.{} (title, description, type, choices)
-                VALUES (?, ?, ?, ?)
-            ",
             created_form_id.id()
         ))
-        .bind::<VarChar, _>(question.title())
-        .bind::<VarChar, _>(question.description())
-        .bind::<VarChar, _>(question.question_type().to_string())
-        .bind::<Nullable<Text>, _>(choices)
         .execute(connection)
-        .is_ok()
+        .is_ok();
+
+        let mut insert_state = _form.questions().iter().map(|question| {
+            let choices = question.choices().clone().map(|choices| choices.join(","));
+            sql_query(format!(
+                r"INSERT INTO forms.{} (title, description, type, choices)
+                VALUES (?, ?, ?, ?)
+            ",
+                created_form_id.id()
+            ))
+            .bind::<VarChar, _>(question.title())
+            .bind::<VarChar, _>(question.description())
+            .bind::<VarChar, _>(question.question_type().to_string())
+            .bind::<Nullable<Text>, _>(choices)
+            .execute(connection)
+            .is_ok()
+        });
+
+        let database_process_state =
+            is_form_inserted && is_success_create_table && insert_state.all(|rs| rs == true);
+
+        if database_process_state {
+            Ok(())
+        } else {
+            Err(Error::RollbackTransaction)
+        }
     });
-    is_form_inserted && is_success_create_table && insert_state.all(|rs| rs == true)
+
+    match transaction {
+        Err(_) => false,
+        Ok(_) => true,
+    }
 }
 
 /// formを削除する
