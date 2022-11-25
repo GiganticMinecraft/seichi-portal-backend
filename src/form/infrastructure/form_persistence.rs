0b3130290f8d8e0b98c75c2fcf6d8e60fbdd62de
rito528 <rotoyutoriapp@gmail.com>
2022-11-25 18:05:58 +0900

    tmp: コンパイルは通るが動いていない

--- src/form/infrastructure/form_persistence.rs
+++ src/form/infrastructure/form_persistence.rs
@@ -2,8 +2,8 @@ use crate::database::connection::database_connection;
 use crate::form::handlers::domain_for_user_input::raw_form::RawForm;
 use crate::form::handlers::domain_for_user_input::raw_form_id::RawFormId;
 use sea_orm::DatabaseBackend::MySql;
-use sea_orm::{ConnectionTrait, DbErr, Statement, TransactionTrait};
-use std::pin::Pin;
+use sea_orm::{ConnectionTrait, DbErr, Statement, TransactionTrait, Value};
+use std::iter::Map;
 
 /// formを生成する
 pub async fn create_form(form: RawForm) {
@@ -15,8 +15,75 @@ pub async fn create_form(form: RawForm) {
                 &*"INSERT INTO seichi_portal.forms (name) VALUES (?)".to_owned(),
                 vec![form.form_name().to_owned().into()],
             ))
-            .await
-            .map(|_| ())
+            .await?;
+
+            let created_form_id: i32 = txn
+                .query_one(Statement::from_string(
+                    MySql,
+                    "SELECT LAST_INSERT_ID() AS id".to_owned(),
+                ))
+                .await?
+                .unwrap()
+                .try_get("", "id")?;
+
+            txn.execute(Statement::from_string(
+                MySql,
+                format!(
+                    r"CREATE TABLE forms.{} (
+                    id INT AUTO_INCREMENT,
+                    title VARCHAR(100) NOT NULL,
+                    description VARCHAR(300) NOT NULL,
+                    type VARCHAR(10) NOT NULL,
+                    choices TEXT,
+                    PRIMARY KEY(id)
+                    )",
+                    created_form_id
+                ),
+            ))
+            .await?;
+            // let selialized_form_data = form.questions().into_iter().map(|question| {
+            //     vec![
+            //         question.title(),
+            //         question.description(),
+            //         &question.question_type().to_owned().to_string(),
+            //         Value::String(Box::new(
+            //             question.choices().clone().map(|choices| choices.join(",")),
+            //         )),
+            //     ]
+            // });
+
+            let serialized_questions = form
+                .questions()
+                .iter()
+                .map(|question| {
+                    let b = Value::String(Some(Box::from(question.title().to_owned())));
+                    let c = Value::String(Some(Box::from(question.description().to_owned())));
+                    let d = Value::String(Some(Box::from(
+                        question.question_type().to_owned().to_string().to_owned(),
+                    )));
+                    let f = question
+                        .choices()
+                        .clone()
+                        .map(|choices| Box::from(choices.join(",")));
+                    let e = Value::String(f);
+
+                    vec![b, c, d, e]
+                })
+                .collect::<Vec<Vec<Value>>>();
+
+            serialized_questions.iter().for_each(|question| {
+                txn.execute(Statement::from_sql_and_values::<Vec<Value>>(
+                    MySql,
+                    &*format!(
+                        r"INSERT INTO forms.{} (title, description, type, choices)
+                    VALUES (?, ?, ?, ?)",
+                        created_form_id
+                    ),
+                    question.to_owned().into(),
+                ));
+            });
+
+            Ok(())
         })
     });
 
