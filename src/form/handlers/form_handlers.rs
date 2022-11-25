f2121cffd64645d855508ecd51a748ad28ce77ff
rito528 <rotoyutoriapp@gmail.com>
2022-11-26 02:32:31 +0900

    fix: forms -> form

--- src/form/handlers/form_handlers.rs
+++ src/form/handlers/form_handlers.rs
@@ -2,7 +2,7 @@ use crate::form::handlers::domain_for_user_input::{raw_form::RawForm, raw_form_i
 use crate::form::infrastructure::{create_form, delete_form};
 use actix_web::{post, web::Json, HttpResponse, Responder};
 
-#[post("/api/forms/create")]
+#[post("/api/form/create")]
 pub async fn create_form_handler(info: Json<RawForm>) -> impl Responder {
     let form = info.0;
     match create_form(form).await {
@@ -16,7 +16,7 @@ pub async fn create_form_handler(info: Json<RawForm>) -> impl Responder {
     // }
 }
 
-#[post("/api/forms/delete")]
+#[post("/api/form/delete")]
 pub async fn delete_form_handler(info: Json<RawFormId>) -> impl Responder {
     println!("{:?}", info.0.id());
     delete_form(info.0);
