c570d1686014401931c85cc122e921806aa1e83c
rito528 <rotoyutoriapp@gmail.com>
2022-12-11 21:33:26 +0900

    feat: create_from_answer_type_tableを追加

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
 mod m20221127_173808_form_questions;
+mod m20221211_211233_create_form_answer_type_table;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20221127_173808_form_questions::Migration),
+            Box::new(m20221211_211233_create_form_answer_type_table::Migration),
         ]
     }
 }
