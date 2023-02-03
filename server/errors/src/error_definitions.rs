ee6326a54d8fcce82b32d3bb3c9088127f0c8bc6
rito528 <rotoyutoriapp@gmail.com>
2023-02-03 18:26:12 +0900

    feat: DataBaseErrorを定義

--- server/errors/src/error_definitions.rs
+++ server/errors/src/error_definitions.rs
@@ -1,8 +1,15 @@
 use strum_macros::Display;
+use thiserror::Error;
 
 #[derive(Display)]
 pub enum Error {
     DbTransactionConstructionError,
     SqlExecutionError,
     MutexCanNotUnlock,
+}
+
+#[derive(Error)]
+enum DataBaseError {
+    #[error("line {}: Transaction begin failed.", .linenum)]
+    TransactionBeginFailed
 }
