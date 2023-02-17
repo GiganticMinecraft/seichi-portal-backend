7232b1e9a3bebd57ffaaa2aa71f895538d584675
rito528 <rotoyutoriapp@gmail.com>
2023-02-17 16:49:19 +0900

    fix: anywhere::Resultが使えるように

--- server/errors/src/anywhere.rs
+++ server/errors/src/anywhere.rs
@@ -1,40 +1,38 @@
 use anyhow;
 use tracing;
 
-mod anywhere {
-    use thiserror::{Error};
+use thiserror::{Error};
 
-    #[derive(Error, Debug)]
-    #[error(transparent)]
-    struct Source(#[from] anyhow::Error);
+#[derive(Error, Debug)]
+#[error(transparent)]
+struct Source(#[from] anyhow::Error);
 
-    #[derive(Debug)]
-    pub struct Error {
-        source: Source,
-    }
-
-    impl std::error::Error for Error {
-        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-            self.source.source()
-        }
-    }
-
-    impl std::fmt::Display for Error {
-        fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            writeln!(fmt, "cause:\n\t{}", self.source)
-        }
-    }
-
-    impl<E: std::fmt::Debug> From<E> for Error
-        where
-            Source: From<E>,
-    {
-        fn from(source: E) -> Self {
-            let source: Source = source.into();
-            tracing::error!("{}", &source);
-            Self { source }
-        }
-    }
-
-    pub type Result<T> = std::result::Result<T, Error>;
+#[derive(Debug)]
+pub struct Error {
+    source: Source,
 }
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        self.source.source()
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(fmt, "cause:\n\t{}", self.source)
+    }
+}
+
+impl<E: std::fmt::Debug> From<E> for Error
+    where
+        Source: From<E>,
+{
+    fn from(source: E) -> Self {
+        let source: Source = source.into();
+        tracing::error!("{}", &source);
+        Self { source }
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
