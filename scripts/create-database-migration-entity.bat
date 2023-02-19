b503a872d8d117f20b36892f65c75e79f0566da6
rito528 <rotoyutoriapp@gmail.com>
2023-02-20 00:03:53 +0900

    fix: create-database-migration-entity.batがdbにアクセスできないのを修正

--- scripts/create-database-migration-entity.bat
+++ scripts/create-database-migration-entity.bat
@@ -3,4 +3,4 @@ CALL cd /d %~dp0
 
 REM see https://www.sea-ql.org/SeaORM/docs/next/generate-entity/sea-orm-cli/
 
-CALL sea-orm-cli generate entity -o ./server/database/src/entities/
+CALL sea-orm-cli generate entity --database-url mysql://root:root@localhost/seichi_portal -o ../server/database/src/entities/
