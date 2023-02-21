d37303e261eb956bf8db1a5ccbb5ef779ba0c917
rito528 <rotoyutoriapp@gmail.com>
2023-02-21 23:31:36 +0900

    fix: create_formの処理をchoicesテーブルの存在を利用する処理に

--- server/form/src/infrastructure/form_persistence.rs
+++ server/form/src/infrastructure/form_persistence.rs
@@ -2,14 +2,20 @@ use crate::handlers::domain_for_user_input::raw_form::RawForm;
 use crate::handlers::domain_for_user_input::raw_form_id::RawFormId;
 use crate::handlers::FormHandlers;
 use database::connection::database_connection;
-use database::entities::{form_questions, forms};
+use database::entities::{form_choices, form_questions, forms};
 use sea_orm::ActiveValue::Set;
-use sea_orm::{ActiveModelTrait, EntityTrait, QuerySelect, QueryTrait, TransactionTrait};
+use sea_orm::{
+    ActiveModelTrait, ActiveValue, EntityTrait, InsertResult, QuerySelect, QueryTrait,
+    TransactionTrait,
+};
 
 use crate::domain::{from_string, Form, FormId, FormName, Question};
-use std::sync::Arc;
 use errors::anywhere;
 use errors::error_definitions::FormInfraError;
+use itertools::Itertools;
+use sea_orm::sea_query::ColumnSpec::Default;
+use sea_orm::TryGetError::DbErr;
+use std::sync::Arc;
 
 /// formを生成する
 pub async fn create_form(form: RawForm, handler: Arc<FormHandlers>) -> anywhere::Result<RawFormId> {
@@ -18,7 +24,7 @@ pub async fn create_form(form: RawForm, handler: Arc<FormHandlers>) -> anywhere:
     let txn = connection.begin().await?;
 
     let form_id = forms::ActiveModel {
-        id: Default::default(),
+        id: ActiveValue::NotSet,
         name: Set(form.form_name().to_owned()),
     }
     .insert(&txn)
@@ -28,21 +34,62 @@ pub async fn create_form(form: RawForm, handler: Arc<FormHandlers>) -> anywhere:
     let questions = form
         .questions()
         .iter()
-        .map(|question| form_questions::ActiveModel {
-            question_id: Default::default(),
-            form_id: Set(form_id),
-            title: Set(question.title().to_owned()),
-            description: Set(question.description().to_owned()),
-            answer_type: Set(question.question_type().to_string()),
-            choices: Set(question.choices().clone().map(|choices| choices.join(","))),
-        })
-        .collect::<Vec<form_questions::ActiveModel>>();
+        .map(|question| {
+            let form_questions = form_questions::ActiveModel {
+                question_id: ActiveValue::NotSet,
+                form_id: Set(form_id),
+                title: Set(question.title().to_owned()),
+                description: Set(question.description().to_owned()),
+                answer_type: Set(question.question_type().to_string()),
+            };
 
-    form_questions::Entity::insert_many(questions)
+            let form_choices = question.choices.clone().map(|choices| {
+                choices
+                    .iter()
+                    .map(|choice| form_choices::ActiveModel {
+                        id: ActiveValue::NotSet,
+                        question_id: form_questions.question_id.clone(),
+                        choice: Set(choice.to_string()),
+                    })
+                    .collect_vec()
+            });
+
+            (form_questions, form_choices)
+        })
+        .collect_vec();
+
+    let form_questions = questions
+        .iter()
+        .map(|(question, _)| question.clone().to_owned())
+        .collect_vec();
+
+    form_questions::Entity::insert_many(form_questions)
         .exec(&txn)
         .await?;
+
+    let form_choices = questions
+        .iter()
+        .map(|(_, choices)| choices.clone().to_owned())
+        .collect::<Option<Vec<_>>>()
+        .map(|choices| {
+            choices
+                .iter()
+                .flatten()
+                .map(|choices| choices.clone().to_owned())
+                .collect_vec()
+        });
+
+    if form_choices.is_some() {
+        form_choices::Entity::insert_many(form_choices.unwrap())
+            .exec(&txn)
+            .await?;
+    }
+
     {
-        let mut forms = handler.forms.lock().map_err(|_| FormInfraError::MutexLockFailed)?;
+        let mut forms = handler
+            .forms
+            .lock()
+            .map_err(|_| FormInfraError::MutexLockFailed)?;
 
         forms.push(form.to_form(form_id));
         forms
@@ -58,47 +105,48 @@ pub async fn create_form(form: RawForm, handler: Arc<FormHandlers>) -> anywhere:
 /// 作成されているフォームの取得
 /// 取得に失敗した場合はpanicします
 pub async fn fetch_forms() -> Vec<Form> {
-    let _connection = database_connection().await;
-
-    let txn = _connection
-        .begin()
-        .await
-        .unwrap_or_else(|_| panic!("データベースのトランザクション確立に失敗しました。"));
-
-    forms::Entity::find()
-        .find_with_related(form_questions::Entity)
-        .all(&txn)
-        .await
-        .unwrap_or_else(|_| panic!("フォーム情報の取得に失敗しました"))
-        .iter()
-        .map(|(form_info, questions)| {
-            let form_name = FormName::builder().name(form_info.clone().name).build();
-            let form_id = FormId::builder().form_id(form_info.id).build();
-            let questions =
-                questions
-                    .iter()
-                    .map(|question| {
-                        let question_info = question.clone();
-                        match from_string(question_info.answer_type) {
-                            Some(question_type) => Question::builder()
-                                .title(question_info.title)
-                                .description(question_info.description)
-                                .question_type(question_type)
-                                .choices(question_info.choices.map(|choice| {
-                                    choice.split(',').map(|s| s.to_string()).collect()
-                                }))
-                                .build(),
-                            None => panic!("question_typeのデシリアライズに失敗しました"),
-                        }
-                    })
-                    .collect::<Vec<Question>>();
-            Form::builder()
-                .name(form_name)
-                .id(form_id)
-                .questions(questions)
-                .build()
-        })
-        .collect::<Vec<Form>>()
+    todo!()
+    // let _connection = database_connection().await;
+    //
+    // let txn = _connection
+    //     .begin()
+    //     .await
+    //     .unwrap_or_else(|_| panic!("データベースのトランザクション確立に失敗しました。"));
+    //
+    // forms::Entity::find()
+    //     .find_with_related(form_questions::Entity)
+    //     .all(&txn)
+    //     .await
+    //     .unwrap_or_else(|_| panic!("フォーム情報の取得に失敗しました"))
+    //     .iter()
+    //     .map(|(form_info, questions)| {
+    //         let form_name = FormName::builder().name(form_info.clone().name).build();
+    //         let form_id = FormId::builder().form_id(form_info.id).build();
+    //         let questions =
+    //             questions
+    //                 .iter()
+    //                 .map(|question| {
+    //                     let question_info = question.clone();
+    //                     match from_string(question_info.answer_type) {
+    //                         Some(question_type) => Question::builder()
+    //                             .title(question_info.title)
+    //                             .description(question_info.description)
+    //                             .question_type(question_type)
+    //                             .choices(question_info.choices.map(|choice| {
+    //                                 // choice.split(',').map(|s| s.to_string()).collect()
+    //                             }))
+    //                             .build(),
+    //                         None => panic!("question_typeのデシリアライズに失敗しました"),
+    //                     }
+    //                 })
+    //                 .collect::<Vec<Question>>();
+    //         Form::builder()
+    //             .name(form_name)
+    //             .id(form_id)
+    //             .questions(questions)
+    //             .build()
+    //     })
+    //     .collect::<Vec<Form>>()
 }
 
 /// formを削除する
