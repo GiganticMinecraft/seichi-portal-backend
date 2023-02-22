9e3d25755082259300f3060398a569be4be96281
rito528 <rotoyutoriapp@gmail.com>
2023-02-22 13:32:39 +0900

    fix: tracing::errorを使ってログを出力する

--- server/form/src/handlers/form_handlers.rs
+++ server/form/src/handlers/form_handlers.rs
@@ -2,12 +2,14 @@ use crate::domain::Form;
 use crate::handlers::domain_for_user_input::raw_form::RawForm;
 use crate::handlers::domain_for_user_input::raw_form_id::RawFormId;
 use crate::infrastructure::create_form;
+use anyhow::anyhow;
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::Json;
 use serde_json::json;
 use std::sync::{Arc, Mutex};
+use tracing::log::error;
 use typed_builder::TypedBuilder;
 
 #[derive(TypedBuilder)]
@@ -17,12 +19,12 @@ pub struct FormHandlers {
 
 pub async fn create_form_handler(
     State(forms): State<Arc<FormHandlers>>,
-    Json(request_form): Json<RawForm>
+    Json(request_form): Json<RawForm>,
 ) -> impl IntoResponse {
     match create_form(request_form, forms).await {
         Ok(form_id) => (StatusCode::CREATED, json!(form_id).to_string()),
         Err(err) => {
-            println!("{err}");
+            error!("create_form_handler: {}", err);
             (StatusCode::INTERNAL_SERVER_ERROR, "db error".to_owned())
         }
     }
