fbcd0dc8cb28bae03039f10ff4b8943e5309f9a8
rito528 <rotoyutoriapp@gmail.com>
2023-03-03 01:54:52 +0900

    chore: .envに環境変数をまとめる

--- server/database/src/connection.rs
+++ server/database/src/connection.rs
@@ -5,11 +5,12 @@ use std::env;
 pub async fn database_connection() -> DatabaseConnection {
     dotenv().ok();
 
-    let username = env::var("DB_USER").expect("データベースのユーザー名を指定してください。");
-    let password = env::var("DB_PASSWORD").expect("データベースのパスワードを指定してください。");
-    let host = env::var("DB_HOST").expect("データベースのホストを指定してください。");
-    let port = env::var("DB_PORT").expect("データベースの接続ポートを指定してください。");
-    let database_name = env::var("DB_NAME").expect("データベース名を指定してください。");
+    let username = env::var("MYSQL_USER").expect("データベースのユーザー名を指定してください。");
+    let password =
+        env::var("MYSQL_PASSWORD").expect("データベースのパスワードを指定してください。");
+    let host = env::var("MYSQL_HOST").expect("データベースのホストを指定してください。");
+    let port = env::var("MYSQL_PORT").expect("データベースの接続ポートを指定してください。");
+    let database_name = env::var("MYSQL_DATABASE").expect("データベース名を指定してください。");
 
     let database_url = format!("mysql://{username}:{password}@{host}:{port}/{database_name}");
 
