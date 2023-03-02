6a8c87a1296e75a246fcafc9ec4c0dd043171901
rito528 <rotoyutoriapp@gmail.com>
2023-03-03 01:25:49 +0900

    chore: DBの接続情報を分ける

--- server/database/src/connection.rs
+++ server/database/src/connection.rs
@@ -5,8 +5,13 @@ use std::env;
 pub async fn database_connection() -> DatabaseConnection {
     dotenv().ok();
 
-    let database_url =
-        env::var("DATABASE_URL").expect("データべースのURLを.envファイルに設定してください。");
+    let username = env::var("DB_USER").expect("データベースのユーザー名を指定してください。");
+    let password = env::var("DB_PASSWORD").expect("データベースのパスワードを指定してください。");
+    let host = env::var("DB_HOST").expect("データベースのホストを指定してください。");
+    let port = env::var("DB_PORT").expect("データベースの接続ポートを指定してください。");
+
+    let database_url = format!("mysql://{username}:{password}@{host}/{port}");
+
     Database::connect(&database_url)
         .await
         .unwrap_or_else(|_| panic!("{database_url} に接続できませんでした。"))
