b537bbfc36f623837211c4b4f966b144bdd90626
mitama <mitama@mitama.tech>
2023-03-03 03:10:20 +0900

    feat: aggregates config variables into some structs

--- server/app/src/main.rs
+++ server/app/src/main.rs
@@ -6,20 +6,22 @@ use database::connection;
 use form::handlers::{create_form_handler, FormHandlers};
 use form::infrastructure::fetch_forms;
 use migration::MigratorTrait;
-use std::env;
 
+use crate::config::HTTP;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 use tower_http::cors::{Any, CorsLayer};
 
+mod config;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let _connection = connection::database_connection().await;
-    migration::Migrator::up(&_connection, None).await.unwrap();
+    migration::Migrator::up(&_connection, None).await?;
 
     let handlers = Arc::new(
         FormHandlers::builder()
-            .forms(Mutex::new(fetch_forms().await.unwrap()))
+            .forms(Mutex::new(fetch_forms().await?))
             .build(),
     );
 
@@ -33,11 +35,12 @@ async fn main() {
                 .allow_headers([CONTENT_TYPE]),
         );
 
-    let http_port = env::var("HTTP_PORT").expect("HTTP_PORTを指定してください。");
+    let addr = SocketAddr::from(([0, 0, 0, 0], HTTP.port.parse().unwrap()));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], http_port.parse().unwrap()));
     axum::Server::bind(&addr)
         .serve(router.into_make_service())
         .await
-        .unwrap();
+        .expect("Fail to serve.");
+
+    Ok(())
 }
