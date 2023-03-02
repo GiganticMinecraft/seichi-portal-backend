f0b84535b72516690b7255edf13ab530c5a8c347
rito528 <rotoyutoriapp@gmail.com>
2023-03-03 01:31:38 +0900

    chore: database_nameをenvで指定できるようにする

--- server/database/src/connection.rs
+++ server/database/src/connection.rs
@@ -9,8 +9,9 @@ pub async fn database_connection() -> DatabaseConnection {
     let password = env::var("DB_PASSWORD").expect("データベースのパスワードを指定してください。");
     let host = env::var("DB_HOST").expect("データベースのホストを指定してください。");
     let port = env::var("DB_PORT").expect("データベースの接続ポートを指定してください。");
+    let database_name = env::var("DB_NAME").expect("データベース名を指定してください。");
 
-    let database_url = format!("mysql://{username}:{password}@{host}/{port}");
+    let database_url = format!("mysql://{username}:{password}@{host}:{port}/{database_name}");
 
     Database::connect(&database_url)
         .await
