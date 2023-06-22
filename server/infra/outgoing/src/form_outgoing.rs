2c4b95446d03b3463db5aa92cb9c144af0a34d82
rito528 <39003544+rito528@users.noreply.github.com>
2023-06-23 00:14:02 +0900

    fix: webhook通知部分のコンパイルエラーを直す

--- server/infra/outgoing/src/form_outgoing.rs
+++ server/infra/outgoing/src/form_outgoing.rs
@@ -1,50 +1,49 @@
+use anyhow::anyhow;
 use domain::form::models::Form;
 use webhook::client::WebhookClient;
 
 async fn create(form: Form) -> anyhow::Result<()> {
-    match form.settings().webhook_url() {
-        Some(url) => {
-            let client = WebhookClient::new(url);
-            client
-                .send(|message| {
-                    message.username("seichi-portal-backend").embed(|embed| {
-                        embed
-                            .title("フォームが作成されました")
-                            .field("フォーム名", form.title().title(), false)
-                            .field(
-                                "フォーム説明",
-                                &form
-                                    .description()
-                                    .unwrap_or("フォームの説明は設定されていません。".to_string()),
-                                false,
-                            )
-                    })
+    if let Some(url) = form.settings().webhook_url() {
+        let client = WebhookClient::new(url);
+        client
+            .send(|message| {
+                message.username("seichi-portal-backend").embed(|embed| {
+                    embed
+                        .title("フォームが作成されました")
+                        .field("フォーム名", form.title().title(), false)
+                        .field(
+                            "フォーム説明",
+                            &form
+                                .description()
+                                .description()
+                                .to_owned()
+                                .unwrap_or("フォームの説明は設定されていません。".to_string()),
+                            false,
+                        )
                 })
-                .await?
-        }
-        _ => (),
+            })
+            .await
+            .map_err(|_| anyhow!("Failed to notify form creation via webhook."))?;
     }
 
     Ok(())
 }
 
 async fn delete(form: Form) -> anyhow::Result<()> {
-    match form.settings().webhook_url() {
-        Some(url) => {
-            let client = WebhookClient::new(url);
-            client
-                .send(|message| {
-                    message.username("seichi-portal-backend").embed(|embed| {
-                        embed.title("フォームが削除されました。").field(
-                            "フォーム名",
-                            form.title().title(),
-                            false,
-                        )
-                    })
+    if let Some(url) = form.settings().webhook_url() {
+        let client = WebhookClient::new(url);
+        client
+            .send(|message| {
+                message.username("seichi-portal-backend").embed(|embed| {
+                    embed.title("フォームが削除されました。").field(
+                        "フォーム名",
+                        form.title().title(),
+                        false,
+                    )
                 })
-                .await?
-        }
-        _ => (),
+            })
+            .await
+            .map_err(|_| anyhow!("Failed to notify form deletion via webhook."))?;
     }
 
     Ok(())
