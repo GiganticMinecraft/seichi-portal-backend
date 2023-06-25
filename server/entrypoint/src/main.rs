e532116033bf0f629c27f81e517a4025ef6a8473
rito528 <39003544+rito528@users.noreply.github.com>
2023-06-25 15:41:13 +0900

    feat: update_form_handlerをrouterに登録する

--- server/entrypoint/src/main.rs
+++ server/entrypoint/src/main.rs
@@ -5,6 +5,7 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use presentation::form_handler::update_form_handler;
 use presentation::{
     form_handler::{create_form_handler, delete_form_handler, form_list_handler, get_form_handler},
     health_check_handler::health_check,
@@ -63,14 +64,16 @@ async fn main() -> anyhow::Result<()> {
         .with_state(shared_repository.to_owned())
         .route(
             "/forms/:id",
-            get(get_form_handler).delete(delete_form_handler),
+            get(get_form_handler)
+                .delete(delete_form_handler)
+                .patch(update_form_handler),
         )
         .with_state(shared_repository.to_owned())
         .route("/health", get(health_check))
         .layer(layer)
         .layer(
             CorsLayer::new()
-                .allow_methods([Method::GET, Method::POST, Method::DELETE])
+                .allow_methods([Method::GET, Method::POST, Method::DELETE, Method::PATCH])
                 .allow_origin(Any) // todo: allow_originを制限する
                 .allow_headers([CONTENT_TYPE]),
         );
