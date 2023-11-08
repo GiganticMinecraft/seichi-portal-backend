984b653639ffa007ef3b1ac25fa87435790ec672
rito528 <39003544+rito528@users.noreply.github.com>
2023-11-08 20:53:29 +0900

    feat: ユーザーのRoleを変更するエンドポイントの実装

--- server/domain/src/repository/user_repository.rs
+++ server/domain/src/repository/user_repository.rs
@@ -1,11 +1,13 @@
 use async_trait::async_trait;
 use errors::Error;
 use mockall::automock;
+use uuid::Uuid;
 
-use crate::user::models::User;
+use crate::user::models::{Role, User};
 
 #[automock]
 #[async_trait]
 pub trait UserRepository: Send + Sync + 'static {
     async fn upsert_user(&self, user: &User) -> Result<(), Error>;
+    async fn patch_user_role(&self, uuid: Uuid, role: Role) -> Result<(), Error>;
 }
--- server/presentation/src/user_handler.rs
+++ server/presentation/src/user_handler.rs
@@ -1,7 +1,40 @@
-use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
-use domain::user::models::User;
+use axum::{
+    extract::{Path, Query, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Extension, Json,
+};
+use domain::{
+    repository::Repositories,
+    user::models::{Role, User},
+};
+use resource::repository::RealInfrastructureRepository;
 use serde_json::json;
+use usecase::user::UserUseCase;
+use uuid::Uuid;
 
 pub async fn get_my_user_info(Extension(user): Extension<User>) -> impl IntoResponse {
     (StatusCode::OK, Json(json!(user))).into_response()
+}
+
+pub async fn patch_user_role(
+    State(repository): State<RealInfrastructureRepository>,
+    Path(uuid): Path<Uuid>,
+    Query(role): Query<Role>,
+) -> impl IntoResponse {
+    let user_use_case = UserUseCase {
+        repository: repository.user_repository(),
+    };
+
+    match user_use_case.patch_user_role(uuid, role).await {
+        Ok(_) => (StatusCode::OK).into_response(),
+        Err(err) => {
+            tracing::error!("{}", err);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({ "reason": "unknown error" })),
+            )
+                .into_response()
+        }
+    }
 }
