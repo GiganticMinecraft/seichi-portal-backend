787aeeaa955889860e483ba8f9eb84d3ed931b86
rito528 <39003544+rito528@users.noreply.github.com>
2023-12-09 23:02:59 +0900

    feat: outgoingにpost時のメッセージ送信機能を追加

--- server/infra/outgoing/src/form_outgoing.rs
+++ server/infra/outgoing/src/form_outgoing.rs
@@ -1,5 +1,6 @@
-use domain::form::models::Form;
+use domain::form::models::{Form, PostedAnswers};
 use errors::infra::InfraError;
+use itertools::Itertools;
 
 use crate::webhook::Webhook;
 
@@ -20,6 +21,46 @@ pub async fn create(form: Form) -> Result<(), InfraError> {
                     .unwrap_or("フォームの説明は設定されていません。".to_string()),
                 false,
             )
+            .send()
+            .await?;
+    }
+
+    Ok(())
+}
+
+pub async fn post(form: &Form, answers: &PostedAnswers) -> Result<(), InfraError> {
+    if let Some(url) = form.settings.webhook_url.to_owned() {
+        Webhook::new(url, "回答が送信されました".to_string())
+            .field(
+                "フォーム名".to_string(),
+                form.title.title().to_owned(),
+                false,
+            )
+            .field(
+                "タイトル".to_string(),
+                answers
+                    .title
+                    .default_answer_title
+                    .to_owned()
+                    .unwrap_or_default(),
+                false,
+            )
+            .field(
+                "内容".to_string(),
+                answers
+                    .answers
+                    .iter()
+                    .map(|answer| {
+                        form.questions
+                            .iter()
+                            .find(|question| question.id == answer.question_id)
+                            .map(|question| question.title.to_owned())
+                            .unwrap_or_else(|| "不明な質問".to_string())
+                    })
+                    .join("\n"),
+                false,
+            )
+            .field("回答者".to_string(), answers.uuid.to_string(), false)
             .send()
             .await?;
     }
