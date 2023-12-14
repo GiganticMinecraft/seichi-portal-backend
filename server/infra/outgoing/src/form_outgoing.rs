9912d52e2bfc0e2543d8733270c999f1361e913e
rito528 <39003544+rito528@users.noreply.github.com>
2023-12-14 23:47:52 +0900

    feat: webhook送信部分を実装

--- server/infra/outgoing/src/form_outgoing.rs
+++ server/infra/outgoing/src/form_outgoing.rs
@@ -1,7 +1,9 @@
-use domain::form::models::Form;
+use domain::form::models::{Comment, Form, PostedAnswers, PostedAnswersSchema};
+use domain::user::models::User;
 use errors::infra::InfraError;
+use itertools::Itertools;
 
-use crate::webhook::Webhook;
+use crate::webhook::{Color, Webhook};
 
 #[tracing::instrument]
 pub async fn create(form: Form) -> Result<(), InfraError> {
@@ -20,7 +22,75 @@ pub async fn create(form: Form) -> Result<(), InfraError> {
                     .unwrap_or("フォームの説明は設定されていません。".to_string()),
                 false,
             )
-            .send()
+            .send(Color::Aqua)
+            .await?;
+    }
+
+    Ok(())
+}
+
+pub async fn post_answer(
+    form: &Form,
+    user: &User,
+    answers: &PostedAnswersSchema,
+) -> Result<(), InfraError> {
+    if let Some(url) = form.settings.webhook_url.to_owned() {
+        Webhook::new(url, "回答が送信されました".to_string())
+            .field(
+                "フォーム名".to_string(),
+                form.title.title().to_owned(),
+                false,
+            )
+            .field(
+                "タイトル".to_string(),
+                answers
+                    .title
+                    .default_answer_title
+                    .to_owned()
+                    .unwrap_or_default(),
+                false,
+            )
+            .fields(
+                answers
+                    .answers
+                    .iter()
+                    .map(|answer| {
+                        (
+                            form.questions
+                                .iter()
+                                .find(|question| question.id == answer.question_id)
+                                .map(|question| question.title.to_owned())
+                                .unwrap_or("不明な質問".to_string()),
+                            answer.answer.to_owned(),
+                        )
+                    })
+                    .collect_vec(),
+                false,
+            )
+            .field("回答者".to_string(), user.name.to_owned(), false)
+            .send(Color::Lime)
+            .await?;
+    }
+
+    Ok(())
+}
+
+#[tracing::instrument]
+pub async fn post_comment(
+    form: &Form,
+    comment: &Comment,
+    answer: &PostedAnswers,
+) -> Result<(), InfraError> {
+    if let Some(url) = form.settings.webhook_url.webhook_url.to_owned() {
+        Webhook::new(url, "回答に対してコメントが投稿されました".to_string())
+            .field("回答".to_string(), answer.title.unwrap_or_default(), false)
+            .field("内容".to_string(), comment.content.to_owned(), false)
+            .field(
+                "発言者".to_string(),
+                comment.commented_by.name.to_owned(),
+                false,
+            )
+            .send(Color::Lime)
             .await?;
     }
 
@@ -36,7 +106,7 @@ pub async fn delete(form: Form) -> Result<(), InfraError> {
                 form.title.title().to_owned(),
                 false,
             )
-            .send()
+            .send(Color::Red)
             .await?;
     }
 
