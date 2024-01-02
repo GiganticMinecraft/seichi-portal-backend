6519836785db54e1486ca15f031907d0a7d14254
mitama <mitama@mitama.tech>
2024-01-02 19:49:33 +0900

    feat: use derving(Copy) in types.rs

--- server/types/src/lib.rs
+++ server/types/src/lib.rs
@@ -5,16 +5,8 @@ use proptest_derive::Arbitrary;
 
 #[derive(DerivingVia, Debug, PartialOrd, PartialEq)]
 #[cfg_attr(feature = "arbitrary", derive(Arbitrary))]
-#[deriving(From, Into, Default, IntoInner(via: i32), Display(via: i32), Serialize(via: i32), Deserialize(via: i32))]
+#[deriving(From, Into, Copy, Default, IntoInner(via: i32), Display(via: i32), Serialize(via: i32), Deserialize(via: i32))]
 pub struct Id<T>(#[underlying] i32, std::marker::PhantomData<T>);
-
-impl<T> Clone for Id<T> {
-    fn clone(&self) -> Self {
-        *self
-    }
-}
-
-impl<T> Copy for Id<T> {}
 
 #[async_trait]
 pub trait Resolver<T, Error, Repo> {
