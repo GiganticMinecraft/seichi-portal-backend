63432fba954942c9f7788a4b9011b3ce551463f2
rito528 <39003544+rito528@users.noreply.github.com>
2024-08-21 13:37:12 +0900

    fix: エラーメッセージのミスを修正

--- server/presentation/src/auth.rs
+++ server/presentation/src/auth.rs
@@ -79,7 +79,7 @@ pub async fn auth(
 
     if user.role == StandardUser
         && !static_endpoints_allowed_for_standard_users
-        .contains(&(request.method(), request.uri().path()))
+            .contains(&(request.method(), request.uri().path()))
         && is_not_allow_dynamic_endpoint
     {
         // NOTE: standard_user_endpointsに存在しないMethodとエンドポイントに
@@ -100,7 +100,7 @@ pub async fn auth(
                     "SEICHI_PORTAL__SESSION_ID={session_id}; Max-Age={half_an_hour}; Path=/; \
                      Secure; HttpOnly"
                 ))
-                    .unwrap(),
+                .unwrap(),
             );
             Ok(response)
         }
