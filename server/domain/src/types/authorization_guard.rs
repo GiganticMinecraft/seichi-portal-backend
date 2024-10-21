653b4aed9e3e84ea6c5b445ddb6c3849f999d6ef
rito528 <39003544+rito528@users.noreply.github.com>
2024-10-21 16:39:25 +0900

    test: AuthorizationGuard のテストを追加

--- server/domain/src/types/authorization_guard.rs
+++ server/domain/src/types/authorization_guard.rs
@@ -58,3 +58,63 @@ pub trait AuthorizationGuardDefinitions<T> {
     fn can_create(&self, actor: &User) -> bool;
     fn can_read(&self, actor: &User) -> bool;
 }
+
+#[cfg(test)]
+mod test {
+    use uuid::Uuid;
+
+    use crate::{
+        types::authorization_guard::{AuthorizationGuard, AuthorizationGuardDefinitions},
+        user::models::{Role, User},
+    };
+
+    #[test]
+    fn authorization_guard_test() {
+        struct AuthorizationGuardTestStruct {
+            pub _value: String,
+        }
+
+        impl AuthorizationGuardDefinitions<AuthorizationGuardTestStruct> for AuthorizationGuardTestStruct {
+            fn can_create(&self, user: &User) -> bool {
+                user.role == Role::Administrator
+            }
+
+            fn can_read(&self, user: &User) -> bool {
+                user.role == Role::Administrator || user.role == Role::StandardUser
+            }
+        }
+
+        let admin = User {
+            name: "admin".to_string(),
+            id: Uuid::new_v4(),
+            role: Role::Administrator,
+        };
+
+        let standard_user = User {
+            name: "standard_user".to_string(),
+            id: Uuid::new_v4(),
+            role: Role::StandardUser,
+        };
+
+        let guard_by_admin = AuthorizationGuard::try_new(
+            &admin,
+            AuthorizationGuardTestStruct {
+                _value: "test".to_string(),
+            },
+        );
+
+        let guard_by_standard_user = AuthorizationGuard::try_new(
+            &standard_user,
+            AuthorizationGuardTestStruct {
+                _value: "test".to_string(),
+            },
+        );
+
+        assert!(&guard_by_admin.is_ok());
+        assert!(&guard_by_standard_user.is_err());
+
+        let read_guard = guard_by_admin.unwrap().into_read();
+        assert!(&read_guard.try_read(&admin).is_ok());
+        assert!(&read_guard.try_read(&standard_user).is_ok())
+    }
+}
