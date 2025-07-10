fe088e6d121765e022bb17210a0ed1993979aad7
rito528 <39003544+rito528@users.noreply.github.com>
2025-07-10 15:21:04 +0900

    fix: 通知設定を更新するエンドポイントで不要なスキーマを受け取らないようにする

--- server/presentation/src/schemas/notification/notification_request_schemas.rs
+++ server/presentation/src/schemas/notification/notification_request_schemas.rs
@@ -1,8 +1,6 @@
 use serde::Deserialize;
-use uuid::Uuid;
 
 #[derive(Deserialize, Debug)]
 pub struct NotificationSettingsUpdateSchema {
-    pub recipient_id: Uuid,
     pub is_send_message_notification: bool,
 }
